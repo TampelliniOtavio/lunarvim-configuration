314b3a7546ba17fe134f44edbf99747ab1b11e51
Otavio Tampellini <tampelliniotavio@gmail.com>
2024-06-20 12:16:38 -0300

    Configurando tailwindcss

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,3 +1,18 @@
 lvim.lsp.automatic_servers_installation = false
 
 require("lvim.lsp.manager").setup("marksman")
+
+require("lvim.lsp.manager").setup("tailwindcss", {
+  filetypes = {
+    "html",
+    "vue",
+    "typescriptreact",
+    "javascriptreact",
+    "css",
+    "blade",
+    "astro"
+  },
+  tailwindCSS = {
+    emmetCompletions = true
+  }
+})
